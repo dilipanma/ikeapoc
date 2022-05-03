74c9b2129b3ec00f4eeda2c2004494d9d8d73ba9
Dilipan Muthuramalingam <dilipan.muthuramalingam@ingka.com>
2022-05-03 18:10:24 +0200

    Updating the CF changes

--- cloud_functions/ikeatwitterpoc/EnvConfig.py
+++ cloud_functions/ikeatwitterpoc/EnvConfig.py
@@ -1,6 +1,6 @@
 project_id = 'ingka-pd-pysb-dev'
 bucket = 'pd-py-working-bucket'
-localpath = '/home/jupyter/Dilipan/Test/twitter/'
+localpath = '/tmp/'
 blobpath = 'ikeapoc/ikea_twitter.csv'
 dataset_id = 'test_ds'
 table = 'twitter_poc'
@@ -8,3 +8,4 @@ consumer_key = 'f6KOTSFaMgxDG2t89sexUCNz6'
 consumer_secret = '5y0hvVhkhuFNNzzWxVSS8LvSUFtiHEF3KNKFfuzv1H6lHXqb46'
 access_key= '1342350661-XZ68gf8AavfHdaZEorlTrMnu2YiXqxZXsfIwzD9'
 access_secret = 'srzSUmnd2IGLG2P2cFUvte2f6sBUqCNEOMlyJUvmhqC8D'
+uri = "gs://pd-py-working-bucket/ikeapoc/ikea_twitter.csv"
